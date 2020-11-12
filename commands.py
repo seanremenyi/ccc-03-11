e59ec643ef86d35dcd1a6c01e0f00362e7845063
Sean <sean.remenyi@gmail.com>
2020-11-12 00:59:39 +0000

    added faker

--- commands.py
+++ commands.py
@@ -3,6 +3,27 @@ from flask import Blueprint
 
 db_commands = Blueprint("db", __name__)
 
-@db_commands.cli.command("richardsWisdom")
-def wisdom():
-    print("hello world")
+@db_commands.cli.command("create")
+def create_db():
+    db.create_all()
+    print("Tables Created")
+    
+@db_commands.cli.command("drop")
+def drop_db():
+    db.drop_all()
+    print("Tables deleted")
+    
+@db_commands.cli.command("seed")
+def seed_db():
+    from models.Book import Book
+    from faker import Faker
+    faker = Faker()
+    
+    for i in range(10):
+        book = Book()
+        book.title = faker.catch_phrase()
+        db.session.add(book)
+        print(f"{i} record(s) created")
+    
+    db.session.commit()
+    print("Tables seeded")

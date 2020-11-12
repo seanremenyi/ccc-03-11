9b3ee008c7b9f865373f4f95d1e1b2737f4fae3f
Sean <sean.remenyi@gmail.com>
2020-11-12 03:49:01 +0000

    refactored for factory pattern

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 from flask import Flask, jsonify
-app = Flask(__name__)
-app.config.from_object("default_settings.app_config")
-
+from flask_marshmallow import Marshmallow   
 from database import init_db
-db = init_db(app)
+from flask_sqlalchemy import SQLAlchemy
 
-from flask_marshmallow import Marshmallow
-ma = Marshmallow(app)
+db = SQLAlchemy()
+ma = Marshmallow()
 
-from commands import db_commands
-app.register_blueprint(db_commands)
 
-from controllers import registerable_controllers
-
-for controller in registerable_controllers:
-    app.register_blueprint(controller)
+def create_app():    
+    app = Flask(__name__)
+    app.config.from_object("default_settings.app_config")
     
-from marshmallow.exceptions import ValidationError
-
-@app.errorhandler(ValidationError)
-def handle_bad_request(error):
-    return (jsonify(error.messages), 400)
+    # from database import init_db
+    # db = init_db(app)
+    
+    # from flask_marshmallow import Marshmallow
+    # ma = Marshmallow(app)
+    
+    db.init_app(app)
+    ma.init_app(app)
+    
+    from commands import db_commands
+    app.register_blueprint(db_commands)
+    
+    from controllers import registerable_controllers
+    
+    for controller in registerable_controllers:
+        app.register_blueprint(controller)
+        
+    from marshmallow.exceptions import ValidationError
+    
+    @app.errorhandler(ValidationError)
+    def handle_bad_request(error):
+        return (jsonify(error.messages), 400)
+    
+    return app
